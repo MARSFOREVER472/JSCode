3038a973e8a6ebee28fc9a3273343ea1b1df449d
Marcelo Andrés <69094327+MARSFOREVER472@users.noreply.github.com>
2024-06-21 10:29:06 -0400

    Variables agregadas al JavaScript.

---
+++ Ejecutables/Codigo.js
@@ -0,0 +1,16 @@
+// ESTA VEZ VAMOS A CREAR VARIABLES Y SEPARARLAS POR LÍNEAS...
+
+// document.write('Hola a todos!');
+// document.write('Espero que estén muy bien!');
+
+var nombre = 'Pedro'; // NOMBRE 1.
+var nombre2 = 'Anny'; // NOMBRE 2.
+var nombre3 = 'Tomas'; // NOMBRE 3.
+var nombre4 = 'Elizabeth'; // NOMBRE 4.
+var nombre5 = 'Antonio'; // NOMBRE 5.
+
+let saludo = 'hola'; // SALUDO INICIAL.
+let saludo2 = 'que tal?'; // SEGUNDO SALUDO.
+let saludo3 = 'como te va?'; // TERCER SALUDO.
+let saludo4 = 'encantado'; // CUARTO SALUDO.
+let saludo5 = 'buenos dias'; // QUINTO Y ÚLTIMO SALUDO.
