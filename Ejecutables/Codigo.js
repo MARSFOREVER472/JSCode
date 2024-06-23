61556530501a8b1fa15ba8ec8beefe258507259d
Marcelo Andrés <69094327+MARSFOREVER472@users.noreply.github.com>
2024-06-22 23:24:56 -0400

    Falta aprender a concatenar variables...

--- Ejecutables/Codigo.js
+++ Ejecutables/Codigo.js
@@ -14,3 +14,7 @@ let saludo2 = 'que tal?'; // SEGUNDO SALUDO.
 let saludo3 = 'como te va?'; // TERCER SALUDO.
 let saludo4 = 'encantado'; // CUARTO SALUDO.
 let saludo5 = 'buenos dias'; // QUINTO Y ÚLTIMO SALUDO.
+
+// DESPLIEGA EL NOMBRE MÁS UN SALUDO!!!!...
+document.write(nombre + saludo2);
+document.write(saludo2);
